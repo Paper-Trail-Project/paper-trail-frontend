b7ca4ea500743af7f943a5edea9e10ef5a02e1fa
github username <github email address>
2020-06-01 14:21:42 -0600

    added login form and signup form for testing with backend authentication

---
+++ src/Components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Login.css'
+
+export default function Login() {
+    return (
+        <div id="Login-container">
+            <h1>Login</h1>
+            <form id="Login-form">
+                <input className="Login-input" placeholder="Username or Email"></input>
+                <input className="Login-input" placeholder="Password"></input>
+                <button id="Login-button">Login</button>
+            </form>
+            
+        </div>
+    )
+}
