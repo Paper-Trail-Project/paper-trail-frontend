9011acaf562c093ca859b0b0d3f77cf823eba716
github username <github email address>
2020-06-13 09:23:07 -0600

    deployed site to firebase and added background image

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
         <div id="Login-container">
             <h1>Login</h1>
             <form id="Login-form">
-                <input className="Login-input" placeholder="Username or Email"></input>
+                <input className="Login-input" placeholder="Email"></input>
                 <input className="Login-input" placeholder="Password"></input>
                 <button id="Login-button">Login</button>
             </form>
