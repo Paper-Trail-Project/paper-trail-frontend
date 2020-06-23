deac892263ad4eb324d708992f23fe70bb73e516
github username <github email address>
2020-06-23 13:44:25 -0600

    added name and username to localStorage

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import React from 'react';
 import './App.css';
 import Routes from './Components/Routes'
 
-
-
-
-
-
-
 function App() {
   return (
     
-    <div className="App" style={divStyle} >
+    <div className="App" >
       <Routes />
     </div>
   );
