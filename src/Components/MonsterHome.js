16ac0b2e27f7cfe2ad6d52e9ee874f4c76ea70ac
merestrickland <mere.strickland@gmail.com>
2020-06-24 13:01:35 -0400

    flexbox css setup

--- src/Components/MonsterHome.js
+++ src/Components/MonsterHome.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../Styles/MonsterHome.css";
+import monster_droplet from "../Images/monster_droplet.svg";
+import money_icon from "../Images/money_icon.svg";
+import encyclopedia_icon from "../Images/encyclopedia_icon.svg"
+//will have to import all monster images and then update the img src depending on user avatar
 
 export default function MonsterHome() {
   return (
-    <div>
-      This is where my monster will live
+    <div id="container">
+      <div className="knowledge-monster-shop">
+        <div className="icon-container"></div>
+        <div className="icon-container">
+          <img src={encyclopedia_icon} alt="encyclopedia_icon" className="icon"/>
+        </div>
+        <div className="icon-container"></div>
+      </div>
+
+      <div className="knowledge-monster-shop">
+
+        <div id="money-container">
+          <img src={money_icon} alt="money_icon" id="money-icon" />
+        </div>
+
+        <div id="monster-container">
+          <img src={monster_droplet} alt="monster_droplet" id="monster-main" />
+        </div>
+
+      </div>
+
+      <div className="knowledge-monster-shop">
+        <div className="icon-container"></div>
+        <div className="icon-container"></div>
+        <div className="icon-container"></div>
+      </div>
     </div>
-  )
+  );
 }
-
-
